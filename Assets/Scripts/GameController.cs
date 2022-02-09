3eabc1e8c0ed2a4ef4a8902a648749216081ee38
hogehoge <susumitosusyo@hotmail.com>
2022-02-09 11:36:42 +0900

    ハート変わる

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+   
+
+    public void Update()
+    {
+        //rewew score
+        int score = CalcScore();
+        scoreText.text="Score: " +score + "m";
+
+        //renew life panel
+        lifePanel.UpdateLife(nejiko.Life());
+        
+    }
+    int CalcScore()
+    {
+        //nejikoの走行距離をスコアとする。
+        return (int)nejiko.transform.position.z;
+    }
+}
