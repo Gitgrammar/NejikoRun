7fda3e419bfea6d9fcc35ac26d1f952ff5bf6a55
hogehoge <susumitosusyo@hotmail.com>
2022-02-14 11:01:17 +0900

    終わり

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public NejikoController nejiko;
@@ -18,11 +18,31 @@ public class GameController : MonoBehaviour
 
         //renew life panel
         lifePanel.UpdateLife(nejiko.Life());
+
+        //ねじこのライフが０になったらゲームオーバー
+        if (nejiko.Life() <= 0)
+        {
+            //これ以降updateやめる
+            enabled = false;
+
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            //2秒後にreturnTotitle呼び出す。
+            Invoke("ReturnToTitle", 2.0f);
+        }
         
     }
     int CalcScore()
     {
         //nejikoの走行距離をスコアとする。
         return (int)nejiko.transform.position.z;
+    }
+    void ReturnToTitle()
+    {
+        //タイトルシーんに切れ変え
+        SceneManager.LoadScene("Title");
     }
 }
